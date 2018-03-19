49246c7704a6e47b7e698ef7c5ea0d950881915c
Christopher McGregor <chris@mcgregor.io>
2018-03-19 16:17:08 -0700

    Clean up

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,6 @@ export const environment = {
     name: 'dev',
     production: false,
     delegateIps: ['35.199.173.199', '35.227.16.102', '35.230.76.164', '35.230.101.49'],
-    privateKey: '9dc7a0f09dba1ae2fec78c5238a0917208bd6012e335eda0f6bef87bb7a15a30',
-    address: '7777f2b40aacbef5a5127f65418dc5f951280833',
     m2AppName: 'Dispatch',
     m2AppId: '',
     m2AppVersion: '',
